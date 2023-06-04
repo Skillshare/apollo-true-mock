f8bf3e811e9b1ac7c358e0f2f1a89ec31df4a2cf
Joseph Michael Pasque <joseph.pasque@skillshare.com>
2023-06-04 09:46:25 -0500

    fix: cleanup rover introspection

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM oven/bun:0.6.6
+
+RUN apt-get update
+
+RUN apt-get -y install curl
+
+RUN curl -sSL https://rover.apollo.dev/nix/latest | sh
+
+ENV PATH=$PATH:/root/.rover/bin
+
 COPY bun.lockb package.json .
 RUN bun install
 COPY . .
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -n "$ATM_INTROSPECTION_ENDPOINT"]; then
+if [ -n "$ATM_INTROSPECTION_ENDPOINT" ]; then
     echo "Attempting to load schema via introspection at ${ATM_INTROSPECTION_ENDPOINT}";
-    bun run rover graph introspect ${$ATM_INTROSPECTION_ENDPOINT} --output /home/bun/app/schema.graphql;
+    rover graph introspect ${ATM_INTROSPECTION_ENDPOINT} --output /home/bun/app/schema.graphql;
 fi
 
 bun index.ts
